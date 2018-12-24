ab02e60d1be8fd59a4c84a9fd36e0d0546190357
vilislav7 <vilislav.ivanov1991@gmail.com>
2018-12-24 14:27:24 +0200

    Added methods getMonthDays and getWeekDay

--- http_and_html_basic_exercise/awesome_calendar/CalendarPrinter.php
+++ http_and_html_basic_exercise/awesome_calendar/CalendarPrinter.php
@@ -16,4 +16,13 @@ class CalendarPrinter
         $this->months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
         $this->calendar = [];
     }
+
+    private function getMonthDays ($month) : int {
+        return cal_days_in_month(CAL_GREGORIAN, $month, $this->year);
+    }
+
+    private function getWeekDay ($month, $day) : string {
+        $date = "{$this->year}-{$month}-{$day}";
+        return date('l', strtotime($date));
+    }
 }
