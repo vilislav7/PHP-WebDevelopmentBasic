1e0336b848b68266747ccc6176c68498d44bd238
vilislav7 <vilislav.ivanov1991@gmail.com>
2018-12-27 21:57:55 +0200

    Added insert_records.sql for exercise Insert Records - inserted some records in both tables

---
+++ intro_to_MySQL/insert_records.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `towns` (`name`, `id`) VALUES ('Sofia', '1');
+INSERT INTO `towns` (`name`, `id`) VALUES ('Plovdiv', '2');
+INSERT INTO `towns` (`name`, `id`) VALUES ('Varna', '3');
+
+INSERT INTO `minions` (`name`, `age`, `town_id`, `id`) VALUES ('Kevin', '22', '1', '1');
+INSERT INTO `minions` (`name`, `age`, `town_id`, `id`) VALUES ('Bob', '15', '3', '2');
+INSERT INTO `minions` (`name`, `age`, `town_id`, `id`) VALUES ('Steward', null, '2', '3');
