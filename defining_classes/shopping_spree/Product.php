9da03b20957f8f50dbb7cb64d995e1a2e6ef3e07
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-01 18:41:45 +0200

    Added Product.php class for exercise Shopping Spree

---
+++ defining_classes/shopping_spree/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+class Product
+{
+    private $name;
+    private $price;
+
+    public function __construct(string $name, float $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function getPrice() : float
+    {
+        return $this->price;
+    }
+
+
+}
