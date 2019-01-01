e04a0e3ee879b84dd09fcc59bcbfd6180599ae33
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-01 13:15:29 +0200

    Added setters and validation in Box.php

--- defining_classes/class_box/Box.php
+++ defining_classes/class_box/Box.php
@@ -5,13 +5,48 @@ class Box
     private $a;
     private $b;
     private $c;
+    private $error;
 
     public function __construct(float $a,float $b,float $c)
     {
-        $this->a = $a;
-        $this->b = $b;
-        $this->c = $c;
+        $this->error = false;
+
+        $this->setA($a);
+        $this->setB($b);
+        $this->setC($c);
     }
+
+    private function setA(float $a): void
+    {
+        if ($a > 0) {
+            $this->a = $a;
+        } else {
+            $this->error = true;
+            throw new Exception('Length cannot be zero or negative.');
+        }
+    }
+
+    private function setB(float $b): void
+    {
+        if ($b > 0) {
+            $this->b = $b;
+        } else {
+            $this->error = true;
+            throw new Exception('Width cannot be zero or negative.');
+        }
+    }
+
+    private function setC(float $c): void
+    {
+        if ($c > 0) {
+            $this->c = $c;
+        } else {
+            $this->error = true;
+            throw new Exception('Height cannot be zero or negative.');
+        }
+    }
+
+
 
     public function volume() {
         return $this->a * $this->b * $this->c;
@@ -27,14 +62,19 @@ class Box
 
     public function __toString()
     {
-        $surfaceArea = number_format($this->surfaceArea(), 2, '.', '');
-        $lateralSurfaceArea = number_format($this->lateralSurfaceArea(), 2, '.', '');
-        $volume = number_format($this->volume(), 2, '.', '');
+        if (!$this->error) {
+            $surfaceArea = number_format($this->surfaceArea(), 2, '.', '');
+            $lateralSurfaceArea = number_format($this->lateralSurfaceArea(), 2, '.', '');
+            $volume = number_format($this->volume(), 2, '.', '');
 
-        $boxToString = 'Surface Area - ' . $surfaceArea . PHP_EOL;
-        $boxToString .= 'Lateral Surface Area - ' . $lateralSurfaceArea . PHP_EOL;
-        $boxToString .= 'Volume - ' . $volume . PHP_EOL;
+            $boxToString = 'Surface Area - ' . $surfaceArea . PHP_EOL;
+            $boxToString .= 'Lateral Surface Area - ' . $lateralSurfaceArea . PHP_EOL;
+            $boxToString .= 'Volume - ' . $volume . PHP_EOL;
 
-        return $boxToString;
+            return $boxToString;
+        } else {
+            return '';
+        }
     }
 }
+
