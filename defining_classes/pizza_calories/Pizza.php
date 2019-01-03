7e5f13848856ed5e5ff29cd50ba0230420e8dfc6
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-03 19:19:35 +0200

    validate given pizza name through setter

--- defining_classes/pizza_calories/Pizza.php
+++ defining_classes/pizza_calories/Pizza.php
@@ -12,12 +12,27 @@ class Pizza
     /**
      * Pizza constructor.
      * @param string $name
+     * @throws Exception
      */
     public function __construct(string $name)
     {
-        $this->name = $name;
+        $this->setName($name);
         $this->toppings = [];
     }
+
+    /**
+     * @param string $name
+     * @throws Exception
+     */
+    private function setName(string $name): void
+    {
+        if ($name ==='' || strlen($name) > 15) {
+            throw new Exception('Pizza name should be between 1 and 15 symbols.');
+        }
+        $this->name = $name;
+    }
+
+
 
     /** @param Dough $dough */
     public function addDough (Dough $dough) : void {
