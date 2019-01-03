780c221ef7e251a65977865e4e005006744b172f
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-03 23:46:23 +0200

    added topping size - property, setter and validation

--- defining_classes/pizza_calories/Pizza.php
+++ defining_classes/pizza_calories/Pizza.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once 'Dough.php';
+require_once 'Topping.php';
 class Pizza
 {
     /** @var string $name */
     private $name;
+    /** @var int $toppingSize */
+    private $toppingSize;
     /** @var Dough $dough */
     private $dough;
     /** @var Topping[] $toppings */
@@ -12,11 +16,13 @@ class Pizza
     /**
      * Pizza constructor.
      * @param string $name
+     * @param int $toppingSize
      * @throws Exception
      */
-    public function __construct(string $name)
+    public function __construct(string $name, int $toppingSize)
     {
         $this->setName($name);
+        $this->setToppingSize($toppingSize);
         $this->toppings = [];
     }
 
@@ -26,13 +32,22 @@ class Pizza
      */
     private function setName(string $name): void
     {
+        $name = trim($name);
         if ($name ==='' || strlen($name) > 15) {
             throw new Exception('Pizza name should be between 1 and 15 symbols.');
         }
         $this->name = $name;
     }
-
-
+    /**
+     * @param int $toppingSize
+     * @throws Exception
+     */
+    private function setToppingSize (int $toppingSize) : void {
+        if ($toppingSize > 10 || $this->toppingSize < 0) {
+            throw new Exception('Number of toppings should be in range [0..10].');
+        }
+        $this->toppingSize = $toppingSize;
+    }
 
     /** @param Dough $dough */
     public function addDough (Dough $dough) : void {
