f2e1b4d441bef219070dd4f115d43650a5bdb84f
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-03 17:26:35 +0200

    added setters and validation

--- defining_classes/pizza_calories/Topping.php
+++ defining_classes/pizza_calories/Topping.php
@@ -11,10 +11,36 @@ class Topping
      * Topping constructor.
      * @param string $type
      * @param int $weight
+     * @throws Exception
      */
     public function __construct(string $type, int $weight)
     {
-        $this->type = $type;
+        $this->setType($type);
+        $this->setWeight($weight);
+    }
+
+    /**
+     * @param string $type
+     * @throws Exception
+     */
+    public function setType(string $type): void
+    {
+        if ($type === 'Meat' || $type === 'Veggies' || $type === 'Cheese' || $type === 'Sauce' ) {
+            $this->type = $type;
+        } else {
+            throw new Exception("Cannot place {$type} on top of your pizza.");
+        }
+    }
+
+    /**
+     * @param int $weight
+     * @throws Exception
+     */
+    public function setWeight(int $weight): void
+    {
+        if ($weight < 1 || $weight > 50) {
+            throw new Exception("{$this->type} weight should be in the range [1..50]");
+        }
         $this->weight = $weight;
     }
 
