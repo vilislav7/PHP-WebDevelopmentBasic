e87aa6d39f80029b52493241303bc97ab3356ca1
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-05 19:32:49 +0200

    added setters and validation

--- defining_classes/mankind/Human.php
+++ defining_classes/mankind/Human.php
@@ -11,10 +11,42 @@ class Human
     /**
      * @param string $firstName
      * @param string $lastName
+     * @throws Exception
      */
     public function __construct(string $firstName, string $lastName)
     {
+        $this->setFirstName($firstName);
+        $this->setLastName($lastName);
+    }
+
+    /**
+     *@param string $firstName
+     *@throws Exception
+     */
+    private function setFirstName($firstName) : void {
+        if (!ctype_upper($firstName[0])) {
+            throw new Exception('Expected upper case letter! Argument: firstName');
+        }
+
+        if (strlen($firstName) < 4) {
+            throw new Exception('Expected length at least 4 symbols! Argument: firstName');
+        }
+
         $this->firstName = $firstName;
+    }
+    /**
+     *@param string $lastName
+     *@throws Exception
+     */
+    private function setLastName($lastName) : void {
+        if (!ctype_upper($lastName[0])) {
+            throw new Exception('Expected upper case letter! Argument: lastName');
+        }
+
+        if (strlen($lastName) < 4) {
+            throw new Exception('Expected length at least 4 symbols! Argument: firstName');
+        }
+
         $this->lastName = $lastName;
     }
 
