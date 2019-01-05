8cf33ad3acd14049b3c7aa98390325a4d43123a3
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-05 19:24:21 +0200

    added class parent class Human

---
+++ defining_classes/mankind/Human.php
@@ -0,0 +1,38 @@
+<?php
+
+class Human
+{
+    /** @var string $firstName */
+    private $firstName;
+    /** @var string $lastName */
+    private $lastName;
+
+
+    /**
+     * @param string $firstName
+     * @param string $lastName
+     */
+    public function __construct(string $firstName, string $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+
+}
