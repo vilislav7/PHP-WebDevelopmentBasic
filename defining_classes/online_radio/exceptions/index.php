793a3260984d3668112df016c9fb6dea99fbd460
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-07 20:54:11 +0200

    include all exceptions

---
+++ defining_classes/online_radio/exceptions/index.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once 'InvalidSongException.php';
+include_once 'InvalidArtistNameException.php';
+include_once 'InvalidSongNameException.php';
+include_once 'InvalidSongLengthException.php';
+include_once 'InvalidSongMinutesException.php';
+include_once 'InvalidSongSecondsException.php';
