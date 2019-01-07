e0306414c9e3785cfc3f9cf3f3fba9cfb06f6f89
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-07 20:56:23 +0200

    added getPlaylistLength method

--- defining_classes/online_radio/radio/RadioPlaylist.php
+++ defining_classes/online_radio/radio/RadioPlaylist.php
@@ -23,5 +23,32 @@ class RadioPlaylist
         return count($this->songs);
     }
 
+    public function getPlaylistLength() : string {
+        $minutes = 0;
+        $seconds = 0;
+        $hours = 0;
+
+        foreach ($this->songs as $song) {
+            [$songMinutes, $songSeconds] = explode(':', $song->getSongLength());
+            $minutes += (int)$songMinutes;
+            $seconds += (int)$songSeconds;
+        }
+
+        $secondsIntoMinutes = $seconds / 60;
+        $seconds %= 60;
+
+        $minutes += $secondsIntoMinutes;
+        $hours += (int)($minutes / 60);
+        $minutes %= 60;
+
+        $minutes = $minutes < 10 ? "0{$minutes}" : $minutes;
+        $seconds = $seconds < 10 ? "0{$seconds}" : $seconds;
+
+        if ($hours >= 1) {
+            return "{$hours}h {$minutes}m {$seconds}s";
+        }
+
+        return "{$minutes}m {$seconds}s";
+    }
 }
 
