426c1f1f0e693c61a472d8c8912cdf7e312a94d0
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-08 21:43:46 +0200

    added function createAnimal

--- defining_classes/animals/functions.php
+++ defining_classes/animals/functions.php
@@ -3,3 +3,49 @@
 function isEmpty($var) {
     return $var === '';
 }
+
+function createAnimal(string $type, string $name, int $age, $gender) {
+    switch ($type) {
+        case 'Cat' : {
+            try {
+                $cat = new Cat($name, $age, $gender);
+            } catch (Exception $exception) {
+                echo $exception->getMessage();
+            }
+        }
+            break;
+        case 'Dog' : {
+            try {
+                $cat = new Dog($name, $age, $gender);
+            } catch (Exception $exception) {
+                echo $exception->getMessage();
+            }
+        }
+            break;
+        case 'Frog' : {
+            try {
+                $cat = new Frog($name, $age, $gender);
+            } catch (Exception $exception) {
+                echo $exception->getMessage();
+            }
+        }
+            break;
+        case 'Tomcat' : {
+            try {
+                $cat = new Tomcat($name, $age);
+            } catch (Exception $exception) {
+                echo $exception->getMessage();
+            }
+        }
+            break;
+        case 'Kitten' : {
+            try {
+                $cat = new Kitten($name, $age);
+            } catch (Exception $exception) {
+                echo $exception->getMessage();
+            }
+        }
+            break;
+    }
+
+}
