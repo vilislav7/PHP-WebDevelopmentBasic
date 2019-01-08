d4d02deaaa60a942b3f2a8a41a95a8b1402f5981
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-08 21:22:00 +0200

    added setters and validations

--- defining_classes/animals/Animal.php
+++ defining_classes/animals/Animal.php
@@ -1,8 +1,9 @@
 <?php
 
-
+include_once 'functions.php';
 abstract class Animal implements SoundProducible
 {
+    private const EXCEPTION_MSG = 'Invalid input';
     /** @var string $name */
     private $name;
     /** @var int $age */
@@ -15,12 +16,13 @@ abstract class Animal implements SoundProducible
      *@param string $name
      *@param int $age
      *@param string $gender
+     * @throws Exception
      */
     public function __construct(string $name, int $age, string $gender)
     {
-        $this->name = $name;
-        $this->age = $age;
-        $this->gender = $gender;
+        $this->setName($name);
+        $this->setAge($age);
+        $this->setGender($gender);
 
         echo $this->makeSound().PHP_EOL;
         echo $this->__toString().PHP_EOL;
@@ -29,7 +31,7 @@ abstract class Animal implements SoundProducible
     /**
      * @return string
      */
-    public function getName(): string
+    protected function getName(): string
     {
         return $this->name;
     }
@@ -37,7 +39,7 @@ abstract class Animal implements SoundProducible
     /**
      * @return int
      */
-    public function getAge(): int
+    protected function getAge(): int
     {
         return $this->age;
     }
@@ -45,9 +47,45 @@ abstract class Animal implements SoundProducible
     /**
      * @return string
      */
-    public function getGender(): string
+    protected function getGender(): string
     {
         return $this->gender;
+    }
+
+    /**
+     * @param string $name
+     * @throws Exception
+     */
+    private function setName(string $name): void
+    {
+        if (isEmpty($name)) {
+            throw new Exception(self::EXCEPTION_MSG);
+        }
+        $this->name = $name;
+    }
+
+    /**
+     * @param int $age
+     * @throws Exception
+     */
+    private function setAge(int $age): void
+    {
+        if ( $age < 0 || isEmpty($age)) {
+            throw new Exception(self::EXCEPTION_MSG);
+        }
+        $this->age = $age;
+    }
+
+    /**
+     * @param string $gender
+     * @throws Exception
+     */
+    private function setGender(string $gender): void
+    {
+        if (isEmpty($gender)) {
+            throw new Exception(self::EXCEPTION_MSG);
+        }
+        $this->gender = $gender;
     }
 
     public function __toString(): string
