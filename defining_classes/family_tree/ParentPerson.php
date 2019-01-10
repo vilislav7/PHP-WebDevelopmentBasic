69cebbc928ae33324082f4e1c5f23f8eb1b25e23
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-10 21:47:18 +0200

    added ParentPerson class

---
+++ defining_classes/family_tree/ParentPerson.php
@@ -0,0 +1,24 @@
+<?php
+
+class ParentPerson extends FamilyPerson
+{
+    /** @var ChildPerson */
+    private $children;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->children = [];
+    }
+
+    /**
+     *@param ChildPerson $person
+     */
+    public function addPerson(ChildPerson $person) : void {
+        $this->children[] = $person;
+    }
+
+    public function getChildren() : array {
+        return $this->children;
+    }
+}
