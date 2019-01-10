8d9e2349bfddcf688ab18c4ce81339e5816e8b7d
vilislav7 <vilislav.ivanov1991@gmail.com>
2019-01-10 22:49:20 +0200

    logic for adding parent first and last name

--- defining_classes/family_tree/index.php
+++ defining_classes/family_tree/index.php
@@ -15,3 +15,42 @@ if (strpos($firstLine, ' ') >= 0) {
     $person->setBirthDate($firstLine);
 }
 
+while (true) {
+    $line = readline();
+    if ($line === 'End') {
+        break;
+    }
+
+    if (strpos($line,'-') >= 0) {
+//        family tie - person on the left is parent to the person on the right
+        [$parentStr, $childStr] = explode('-', $line);
+        $parent = null;
+        $child = null;
+
+        if (strpos($parentStr, ' ') >= 0) {
+            //parentStr is first name last name
+            [$parentFirstName, $parentLastName] = explode(' ', $parentStr);
+
+            if (array_key_exists($parentFirstName, $parents)) {
+                $parent = $parents[$parentFirstName];
+            } else {
+                $parent = new ParentPerson();
+            }
+
+            $parent->setFirstName($parentFirstName);
+            $parent->setLastName($parentLastName);
+
+        } else {
+            //parentStr is birth date
+        }
+
+        if (strpos($childStr, ' ') >= 0 ) {
+            //childStr is first name last name
+        } else {
+            //childStr is birth date
+        }
+
+    } else {
+//        person info
+    }
+}
